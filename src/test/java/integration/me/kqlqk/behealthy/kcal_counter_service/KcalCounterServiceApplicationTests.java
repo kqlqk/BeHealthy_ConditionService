0c85480cc1d3253f70a4536135cc1f4ef264d94e
kqlqk <kononchuk99999@gmail.com>
2022-09-25 19:12:58 +0300

    init

---
+++ src/test/java/integration/me/kqlqk/behealthy/kcal_counter_service/KcalCounterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package integration.me.kqlqk.behealthy.kcal_counter_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcalCounterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
