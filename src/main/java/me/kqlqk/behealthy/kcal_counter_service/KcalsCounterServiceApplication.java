b8b728cffd7e778ad00fb67dfe4e99cf9d3a1f52
kqlqk <kononchuk99999@gmail.com>
2022-10-25 21:31:45 +0200

    bugfix

--- src/main/java/me/kqlqk/behealthy/kcal_counter_service/KcalsCounterServiceApplication.java
+++ src/main/java/me/kqlqk/behealthy/kcal_counter_service/KcalsCounterServiceApplication.java
@@ -2,18 +2,15 @@ package me.kqlqk.behealthy.kcal_counter_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class KcalsCounterServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(KcalsCounterServiceApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(KcalsCounterServiceApplication.class);
-    }
 }
