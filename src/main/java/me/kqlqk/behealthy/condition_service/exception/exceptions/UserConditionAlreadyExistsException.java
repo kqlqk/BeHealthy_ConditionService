db8f4e194c22a6c8411cbec6e7f74e978979011a
kqlqk <kononchuk99999@gmail.com>
2022-11-20 17:38:05 +0100

    bugfix

--- src/main/java/me/kqlqk/behealthy/condition_service/exception/exceptions/UserConditionAlreadyExistsException.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/exception/exceptions/UserConditionAlreadyExistsException.java
@@ -4,6 +4,6 @@ public class UserConditionAlreadyExistsException extends RuntimeException {
     private static final String USER_CONDITION_ALREADY_EXISTS = "UserConditionAlreadyExists";
 
     public UserConditionAlreadyExistsException(String message) {
-        super(USER_CONDITION_ALREADY_EXISTS + "|" + message);
+        super(USER_CONDITION_ALREADY_EXISTS + " | " + message);
     }
 }
