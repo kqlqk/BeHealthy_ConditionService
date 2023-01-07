4610e42a1f74680fb61a285a26756177a16547a5
kqlqk <kononchuk99999@gmail.com>
2023-01-07 23:20:03 +0100

    add validation
    * was added validation for DailyAteFoodDTO.java, UserConditionDTO.java

--- src/main/java/me/kqlqk/behealthy/condition_service/dto/DailyAteFoodDTO.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/dto/DailyAteFoodDTO.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.kqlqk.behealthy.condition_service.model.DailyAteFood;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,16 +16,46 @@ import java.util.List;
 @NoArgsConstructor
 public class DailyAteFoodDTO {
     private long id;
+
+    private long userId;
+
+    @Pattern(regexp = ".{2,50}", message = "Product name should be between 2 and 50 characters")
     private String name;
+
+    @DecimalMin(value = "0.1", message = "Weight should be > 0")
+    @DecimalMax(value = "9999.9", message = "Weight should be < 10000")
     private double weight;
+
+    @DecimalMin(value = "0.0", message = "Kcals should be > 0")
+    @DecimalMax(value = "999.9", message = "Kcals should be < 1000")
     private double kcals;
+
+    @DecimalMin(value = "0.0", message = "Proteins should be > 0")
+    @DecimalMax(value = "999.9", message = "Proteins should be < 1000")
     private double proteins;
+
+    @DecimalMin(value = "0.0", message = "Fats should be > 0")
+    @DecimalMax(value = "999.9", message = "Fats should be < 1000")
     private double fats;
+
+    @DecimalMin(value = "0.0", message = "Carbs should be > 0")
+    @DecimalMax(value = "999.9", message = "Carbs should be < 1000")
     private double carbs;
+
+    public DailyAteFoodDTO(long userId, String name, double weight, double kcals, double proteins, double fats, double carbs) {
+        this.userId = userId;
+        this.name = name;
+        this.weight = weight;
+        this.kcals = kcals;
+        this.proteins = proteins;
+        this.fats = fats;
+        this.carbs = carbs;
+    }
 
     public static DailyAteFoodDTO convertDailyAteFoodToDailyAteFoodDTO(DailyAteFood dailyAteFood) {
         return new DailyAteFoodDTO(
                 dailyAteFood.getId(),
+                dailyAteFood.getUserId(),
                 dailyAteFood.getName(),
                 dailyAteFood.getWeight(),
                 dailyAteFood.getKcals(),
--- src/main/java/me/kqlqk/behealthy/condition_service/dto/UserConditionDTO.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/dto/UserConditionDTO.java
@@ -8,23 +8,55 @@ import me.kqlqk.behealthy.condition_service.model.enums.Gender;
 import me.kqlqk.behealthy.condition_service.model.enums.Goal;
 import me.kqlqk.behealthy.condition_service.model.enums.Intensity;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserConditionDTO {
     private long id;
+
+    private long userId;
+
     private Gender gender;
+
+    @Min(value = 15, message = "Please use valid age (between 15 and 60)")
+    @Max(value = 60, message = "Please use valid age (between 15 and 60)")
     private int age;
+
+    @Min(value = 150, message = "Please use valid height (between 150 and 200)")
+    @Max(value = 200, message = "Please use valid height (between 150 and 200)")
     private int height;
+
+    @Min(value = 40, message = "Please use valid weight (between 40 and 90)")
+    @Max(value = 150, message = "Please use valid weight (between 40 and 90)")
     private int weight;
+
     private Intensity intensity;
+
     private Goal goal;
+
+    @Min(value = 1, message = "Please use valid fatPercent (between 1 and 50)")
+    @Max(value = 50, message = "Please use valid fatPercent (between 1 and 50)")
     private double fatPercent;
 
+
+    public UserConditionDTO(long userId, Gender gender, int age, int height, int weight, Intensity intensity, Goal goal, double fatPercent) {
+        this.userId = userId;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.intensity = intensity;
+        this.goal = goal;
+        this.fatPercent = fatPercent;
+    }
 
     public static UserConditionDTO convertUserConditionToUserConditionDTO(UserCondition userCondition) {
         return new UserConditionDTO(
                 userCondition.getId(),
+                userCondition.getUserId(),
                 userCondition.getGender(),
                 userCondition.getAge(),
                 userCondition.getHeight(),
