cf1f736366ae4cba8984382391c2b8b92b0be0e1
kqlqk <kononchuk99999@gmail.com>
2023-01-19 13:31:10 +0100

    update exception handler
    * was updated GlobalExceptionHandler.java

--- src/main/java/me/kqlqk/behealthy/condition_service/exception/GlobalExceptionHandler.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/exception/GlobalExceptionHandler.java
@@ -1,18 +1,20 @@
 package me.kqlqk.behealthy.condition_service.exception;
 
 import me.kqlqk.behealthy.condition_service.dto.ExceptionDTO;
-import me.kqlqk.behealthy.condition_service.exception.exceptions.UserConditionAlreadyExistsException;
-import me.kqlqk.behealthy.condition_service.exception.exceptions.UserConditionNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({UserConditionAlreadyExistsException.class, UserConditionNotFoundException.class, IllegalArgumentException.class, NullPointerException.class})
+    @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDTO handle(Exception e) {
         ExceptionDTO exceptionDTO = new ExceptionDTO();
@@ -28,5 +30,15 @@ public class GlobalExceptionHandler {
         exceptionDTO.setInfo("Required request body is missing");
 
         return exceptionDTO;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNonValidDataEx(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> errors.put("info", error.getDefaultMessage()));
+
+        return errors;
     }
 }
