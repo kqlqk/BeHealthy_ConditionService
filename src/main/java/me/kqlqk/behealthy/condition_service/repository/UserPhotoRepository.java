30d2b32d348d5ac06eaf0850feb37ac346a15f05
kqlqk <kononchuk99999@gmail.com>
2023-01-26 13:48:23 +0100

    update UserPhotoRepository
    * was added UserPhotoRepository#existsByUserIdAndPhotoDate, UserPhotoRepository#getByUserIdAndPhotoDate

--- src/main/java/me/kqlqk/behealthy/condition_service/repository/UserPhotoRepository.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/repository/UserPhotoRepository.java
@@ -4,7 +4,13 @@ import me.kqlqk.behealthy.condition_service.model.UserPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface UserPhotoRepository extends JpaRepository<UserPhoto, Long> {
     boolean existsByPhotoPath(String photoPath);
+
+    boolean existsByUserIdAndPhotoDate(long userId, Date photoDate);
+
+    UserPhoto getByUserIdAndPhotoDate(long userId, Date date);
 }
