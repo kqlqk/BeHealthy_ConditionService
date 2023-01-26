0ad9a1c87c2f0afeba035828e54111a59a0296f6
kqlqk <kononchuk99999@gmail.com>
2023-01-26 13:49:31 +0100

    update UserPhotoDTO
    * was added UserPhotoDTO#convertUserPhotoToUserPhotoDTO

--- src/main/java/me/kqlqk/behealthy/condition_service/dto/UserPhotoDTO.java
+++ src/main/java/me/kqlqk/behealthy/condition_service/dto/UserPhotoDTO.java
@@ -3,8 +3,10 @@ package me.kqlqk.behealthy.condition_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import me.kqlqk.behealthy.condition_service.model.UserPhoto;
 
 import javax.validation.constraints.Pattern;
+import java.text.SimpleDateFormat;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +17,14 @@ public class UserPhotoDTO {
 
     @Pattern(regexp = "(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-([2-9][0-9])", message = "You should use the following pattern: 'dd-mm-yy'")
     private String photoDate;
+
+    public static UserPhotoDTO convertUserPhotoToUserPhotoDTO(UserPhoto userPhoto) {
+        UserPhotoDTO userPhotoDTO = new UserPhotoDTO();
+        userPhotoDTO.setUserId(userPhoto.getUserId());
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy");
+        userPhotoDTO.setPhotoDate(dateFormat.format(userPhoto.getPhotoDate()));
+
+        return userPhotoDTO;
+    }
 }
